5099bb8a76ff46ac5b46231c19a36b6ed1dbb35b
Amar Asagekar <amarasagekar@gmail.com>
2023-09-30 18:47:41 +0530

    Social Media starter code

---
+++ SocialMedia/SocialMedia/SocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaApp.swift
+//  SocialMedia
+//
+//  Created by AMAR on 30/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
