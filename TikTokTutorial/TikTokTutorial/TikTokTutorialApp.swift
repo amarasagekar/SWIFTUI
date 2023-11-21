f053ac9914e52d709538b4adb27fa780084900fa
Amar Asagekar <amarasagekar@gmail.com>
2023-11-21 22:56:16 +0530

    added tab bar

---
+++ TikTokTutorial/TikTokTutorial/TikTokTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokTutorialApp.swift
+//  TikTokTutorial
+//
+//  Created by AMAR on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TikTokTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
