2aaf4a157924891da717d31c62c24ecfb6148646
Amar Asagekar <amarasagekar@gmail.com>
2023-12-07 17:17:20 +0530

    added ride type data model

---
+++ UberTutorial/UberTutorial/Models/RideType.swift
@@ -0,0 +1,32 @@
+//
+//  RideType.swift
+//  UberTutorial
+//
+//  Created by AMAR on 07/12/23.
+//
+
+import Foundation
+
+enum  RideType: Int, CaseIterable, Identifiable {
+    case uberX
+    case black
+    case uberXL
+    
+    var id: Int { return rawValue }
+    
+    var description: String {
+        switch self {
+        case .uberX: return "UberX"
+        case .black: return "UberBlack"
+        case .uberXL: return "UberXL"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .uberX: return "uber-x"
+        case .black: return "uber-black"
+        case .uberXL: return "uber-x"
+        }
+    }
+}
