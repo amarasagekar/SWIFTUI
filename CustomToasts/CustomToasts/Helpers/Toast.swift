467363619b3fb26008e6c0a8a3128349b6737ae1
Amar Asagekar <amarasagekar@gmail.com>
2023-12-19 23:33:05 +0530

    Added model for toast

--- CustomToasts/CustomToasts/Helpers/Toast.swift
+++ CustomToasts/CustomToasts/Helpers/Toast.swift
@@ -4,7 +4,7 @@
 //
 //  Created by AMAR on 18/12/23.
 //
-//3:43
+//
 import SwiftUI
 
 struct RootView<Content: View> : View {
@@ -32,6 +32,8 @@ struct RootView<Content: View> : View {
     }
 }
 
+
+
 fileprivate class PassthroughWindow: UIWindow {
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         guard let view = super.hitTest(point, with: event) else { return nil}
@@ -40,14 +42,40 @@ fileprivate class PassthroughWindow: UIWindow {
     }
 }
 
+@Observable
+class Toast{
+    static let shared = Toast()
+    fileprivate var topasts: [ToastModel] = []
+    
+    func present(){
+        
+    }
+}
+
+struct ToastModel: Identifiable {
+    let id: UUID = .init()
+    var title: String
+    var symbol: String?
+    var tint: Color
+    var isUserInteractionEnabled: Bool
+    var timing: ToastTime = .medium
+}
+
+enum ToastTime: CGFloat {
+    case short = 1.0
+    case medium = 2.0
+    case long = 3.5
+}
+
 fileprivate struct Toastgroup: View {
+    var model = Toast.shared
     var body: some View {
         GeometryReader{
             let size = $0.size
             let safeArea = $0.safeAreaInsets
             
             ZStack{
-                
+                Text("\(model.topasts.count)")
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
         }
