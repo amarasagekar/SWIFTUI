185fe0c7e33d06a002322bc3bf1b77a8593de5f3
Amar Asagekar <amarasagekar@gmail.com>
2024-02-03 11:31:08 +0530

    create a Share Sheet Extension for the SwiftUI

--- ShareSheetExtension/Share/ShareViewController.swift
+++ ShareSheetExtension/Share/ShareViewController.swift
@@ -15,27 +15,103 @@ class ShareViewController: UIViewController {
         //Interactive Dismiss Disabled
         isModalInPresentation = true
         
-        let hostingView = UIHostingController(rootView: ShareView(extensionContext: extensionContext))
-        hostingView.view.frame = view.frame
-        view.addSubview(hostingView.view)
+        if let itemProviders = (extensionContext!.inputItems.first as? NSExtensionItem)?.attachments
+        {
+            let hostingView = UIHostingController(rootView: ShareView(itemProviders: itemProviders, extensionContext: extensionContext))
+            hostingView.view.frame = view.frame
+            view.addSubview(hostingView.view)
+        }
     }
 }
 
 fileprivate struct ShareView: View {
+    var itemProviders:[NSItemProvider]
     var extensionContext: NSExtensionContext?
+    
+    // View properties
+    @State private var items: [ImageItem] = []
     var body: some View {
-        VStack(spacing: 15) {
-            Text("Add to favourite")
-                .font(.title3.bold())
-                .frame(maxWidth: .infinity)
-                .overlay(alignment: .leading) {
-                    Button("Cancel") {
-                        
+        GeometryReader {
+            let size = $0.size
+            
+            VStack(spacing: 15) {
+                Text("Add to favourite")
+                    .font(.title3.bold())
+                    .frame(maxWidth: .infinity)
+                    .overlay(alignment: .leading) {
+                        Button("Cancel", action: {
+                            dismiss()
+                        })
+                        .tint(.red)
                     }
-                    .tint(.red)
+                    .padding(.bottom, 10)
+                ScrollView(.horizontal){
+                    LazyHStack(spacing: 10){
+                        ForEach(items) { item in
+                            Image(uiImage: item.previewImage)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: size.width - 30)
+                        }
+                    }
+                    .padding(.horizontal, 15)
+                    .scrollTargetLayout()
                 }
-            Spacer(minLength: 0)
+                .scrollTargetBehavior(.viewAligned)
+                .frame(height: 300)
+                .scrollIndicators(.hidden)
+                
+                
+                /// Save button
+                Button(action: saveItems, label: {
+                    Text("Save")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                        .padding(.vertical, 10)
+                        .frame(maxWidth: .infinity)
+                        .foregroundStyle(.white)
+                        .background(.blue, in: .rect(cornerRadius: 10))
+                        .contentShape(.rect)
+                })
+                Spacer(minLength: 0)
+            }
+            .padding(15)
+            .onAppear(perform: {
+                extractItems(size: size)
+            })
         }
-        .padding(15)
+        
+        
+    }
+    
+    // Extracting Image Data and creating Thumbnail Preview Images
+    func extractItems(size: CGSize){
+        guard items.isEmpty else {return}
+        DispatchQueue.global(qos: .userInteractive).async {
+            for provider in itemProviders {
+                let _ = provider.loadDataRepresentation(for: .image) { data, error in
+                    if let data, let image = UIImage(data: data), let thumbnail = image.preparingThumbnail(of: .init(width: size.width, height: 300)){
+                        DispatchQueue.main.async {
+                            items.append(.init(imageData: data, previewImage: thumbnail))
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func saveItems(){
+        
+    }
+    
+    func dismiss() {
+        extensionContext?.completeRequest(returningItems: [])
+    }
+    
+    private struct ImageItem: Identifiable {
+        let id: UUID = .init()
+        var imageData: Data
+        var previewImage: UIImage
     }
 }
---
+++ ShareSheetExtension/ShareSheetExtension/Model/ImageItem.swift
@@ -0,0 +1,11 @@
+//
+//  ImageItem.swift
+//  ShareSheetExtension
+//
+//  Created by AMAR on 03/02/24.
+//
+
+import SwiftUI
+import SwiftData
+
+
