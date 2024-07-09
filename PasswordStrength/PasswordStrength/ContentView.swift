160cf606fb9b2e6f50e479d525e82e781be61916
“amarasagekar” <amarasagekar@gmail.com>
2024-07-09 22:06:14 +0530

    added initial setup

---
+++ PasswordStrength/PasswordStrength/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PasswordStrength
+//
+//  Created by AMAR on 09/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct CheckText
---
+++ PasswordStrength/PasswordStrength/PasswordStrengthApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordStrengthApp.swift
+//  PasswordStrength
+//
+//  Created by AMAR on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PasswordStrengthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
