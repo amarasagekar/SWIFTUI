f2f3bb0353a6e194820eec1feb6ee002bfa2ac2a
“amarasagekar” <amarasagekar@gmail.com>
2024-07-14 21:54:43 +0530

    Completed the app

--- ExpndableSearchBar/ExpndableSearchBar/View/Home.swift
+++ ExpndableSearchBar/ExpndableSearchBar/View/Home.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct Home: View {
     /// View properties
     @State private var searchText: String = ""
+    @FocusState private var isSearching: Bool
     @State private var activeTab: Tab = .all
     @Environment(\.colorScheme) private var scheme
     @Namespace private var animation
@@ -23,72 +24,105 @@ struct Home: View {
             .safeAreaInset(edge: .top, spacing: 0){
                 ExpandableNavigationBar()
             }
+            .animation(.snappy(duration: 0.3, extraBounce: 0), value: isSearching)
         }
+        .scrollTargetBehavior(CustomScrollTargetBehaviour())
         .background(.gray.opacity(0.15))
+        .contentMargins(.top, 190, for: .scrollIndicators)
     }
     
     
     ///Expandable Navigation bar
     @ViewBuilder
     func ExpandableNavigationBar(_ title: String = "Messages") -> some View{
-        VStack(spacing: 10){
-            ///Title
-            Text(title)
-                .font(.largeTitle.bold())
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.bottom, 10)
+        GeometryReader{ proxy in
+            let minY = proxy.frame(in: .scrollView(axis: .vertical)).minY
+            let scrollViewhgeight = proxy.bounds(of: .scrollView(axis: .vertical))?.height ?? 0
+            let scaleProgress = minY > 0 ? 1 + (max(min(minY / scrollViewhgeight, 1), 0) * 0.5) : 1
+            let progress = isSearching ? 1 : max(min(-minY / 70, 1), 0)
             
-            /// Search Bar
-            HStack(spacing: 12){
-                Image(systemName: "magnifyingglass")
-                    .font(.title3)
+            VStack(spacing: 10){
+                ///Title
+                Text(title)
+                    .font(.largeTitle.bold())
+                    .scaleEffect(scaleProgress, anchor: .topLeading)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.bottom, 10)
                 
-                TextField("Search Conversations", text:$searchText)
-            }
-            .padding(.vertical, 10)
-            .padding(.horizontal, 15)
-            .frame(height: 45)
-            .background{
-                RoundedRectangle(cornerRadius: 25.0)
-                    .fill(.background)
-            }
-            
-            ///Custom Segmented picker
-            ScrollView(.horizontal) {
+                /// Search Bar
                 HStack(spacing: 12){
-                    ForEach(Tab.allCases, id: \.rawValue) { tab in
+                    Image(systemName: "magnifyingglass")
+                        .font(.title3)
+                    
+                    TextField("Search Conversations", text:$searchText)
+                        .focused($isSearching)
+                    
+                    if isSearching {
                         Button(action: {
-                            withAnimation(.snappy) {
-                                activeTab = tab
-                            }
-                        }) {
-                            Text(tab.rawValue)
-                                .font(.callout)
-                                .foregroundStyle(activeTab == tab ? (scheme == .dark ? .black : .white) : Color.primary)
-                                .padding(.vertical,8)
-                                .padding(.horizontal, 15)
-                                .background{
-                                    if activeTab == tab {
-                                        Capsule()
-                                            .fill(Color.primary)
-                                            .matchedGeometryEffect(id: "ACTIVETAB", in: animation)
-                                    } else{
-                                        Capsule()
-                                            .fill(.background)
-                                    }
-                                }
-                        }
-                        .buttonStyle(.plain)
-                        
+                            isSearching = false
+                        }, label: {
+                            Image(systemName: "xmark")
+                                .font(.title3)
+                        })
+                        .transition(.asymmetric(insertion: .push(from: .bottom), removal: .push(from: .top)))
                     }
                 }
+                .foregroundStyle(Color.primary)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 15 - (progress * 15))
+                .frame(height: 45)
+                .clipShape(.capsule)
+                .background{
+                    RoundedRectangle(cornerRadius: 25.0 - (progress * 25))
+                        .fill(.background)
+                        .shadow(color: .gray.opacity(0.25), radius: 5, x: 0, y: 5)
+                        .padding(.top, -progress * 190)
+                        .padding(.bottom, -progress * 65)
+                        .padding(.horizontal, -progress * 15)
+                }
+                
+                ///Custom Segmented picker
+                ScrollView(.horizontal) {
+                    HStack(spacing: 12){
+                        ForEach(Tab.allCases, id: \.rawValue) { tab in
+                            Button(action: {
+                                withAnimation(.snappy) {
+                                    activeTab = tab
+                                }
+                            }) {
+                                Text(tab.rawValue)
+                                    .font(.callout)
+                                    .foregroundStyle(activeTab == tab ? (scheme == .dark ? .black : .white) : Color.primary)
+                                    .padding(.vertical,8)
+                                    .padding(.horizontal, 15)
+                                    .background{
+                                        if activeTab == tab {
+                                            Capsule()
+                                                .fill(Color.primary)
+                                                .matchedGeometryEffect(id: "ACTIVETAB", in: animation)
+                                        } else{
+                                            Capsule()
+                                                .fill(.background)
+                                        }
+                                    }
+                            }
+                            .buttonStyle(.plain)
+                            
+                        }
+                    }
+                }
+                .frame(height: 50)
             }
-            .frame(height: 50)
+            .padding(.top, 25)
+            .safeAreaPadding(.horizontal, 15)
+            .offset(y: minY < 0 || isSearching ? -minY : 0)
+            .offset(y: -progress * 65)
         }
-        .padding(.top, 25)
-        .safeAreaPadding(.horizontal, 15)
+        .frame(height: 190)
         .padding(.bottom, 10)
+        .padding(.bottom, isSearching ? -65 : 0)
     }
+    
     ///Dummy Messgae View
     @ViewBuilder
     func DummyMessageView() -> some View{
@@ -113,6 +147,18 @@ struct Home: View {
         }
     }
     
+}
+
+struct CustomScrollTargetBehaviour: ScrollTargetBehavior {
+    func updateTarget(_ target: inout ScrollTarget, context: TargetContext) {
+        if target.rect.minY < 70 {
+            if target.rect.minY < 35 {
+                target.rect.origin = .zero
+            } else {
+                target.rect.origin = .init(x: 0, y:70)
+            }
+        }
+    }
 }
 
 #Preview {
