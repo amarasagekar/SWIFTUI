4a603366d37f4cb749dc1027ce0f9a0ef6ab4bea
Amar A <amarasagekar@gmail.com>
2024-08-20 23:03:30 +0530

    Added Morphing view

--- Walkthrough+Morphing/Walkthrough+Morphing/View/MorphingSymboleView.swift
+++ Walkthrough+Morphing/Walkthrough+Morphing/View/MorphingSymboleView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 /// Custom Symbole Morphing view
 struct MorphingSymboleView: View {
+    var symbole: String
+    var config: Config
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    struct Config{
+        var font: Font
+        var frame: CGSize
+        var radius: CGFloat
+        var foregroundColor: Color
     }
 }
 
 #Preview {
-    MorphingSymboleView()
+    MorphingSymboleView(symbole: "gearshape.fill", config: .init(font: .system(size: 100, weight: .bold), frame: CGSize(width: 250, height: 200), radius: 15, foregroundColor: .black))
 }
